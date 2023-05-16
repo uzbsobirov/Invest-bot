ac4818a0f2afa91c4d3cb11f9b7d5a7a95c8ca7e
uzbsobirov <asobirov9083@gmail.com>
2023-05-16 14:10:23 +0500

    siuuu

--- handlers/users/lang_data.py
+++ handlers/users/lang_data.py
@@ -9,4 +9,4 @@ async def uz_lang(call: types.CallbackQuery, state: FSMContext):
 
 @dp.callback_query_handler(text="ru", state='*')
 async def uz_lang(call: types.CallbackQuery, state: FSMContext):
-    print(call.data)
+    pass
